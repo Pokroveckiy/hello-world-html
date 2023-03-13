357cc46b270273b05fa59ae60c156d4232b1e4ae
Pokroveckiy <123508616+Pokroveckiy@users.noreply.github.com>
2023-03-13 08:58:51 +0000

    create

---
+++ app/main.js
@@ -0,0 +1,9 @@
+console.log('Pokroveckiy');
+const First= 121;
+const Twise='Cat';
+console.log(First);
+console.log(Twise);
+const newTwise=(First);
+console.log(newTwise);
+let myCat;
+console.log(myCat);
