a0736bcafc46b6d7b3b8db653004441a175af7b6
Pokroveckiy <123508616+Pokroveckiy@users.noreply.github.com>
2023-03-13 10:25:38 +0000

    fix

--- app/main.js
+++ app/main.js
@@ -7,3 +7,36 @@ const newTwise=(First);
 console.log(newTwise);
 let myCat;
 console.log(myCat);
+// завдання 6
+let firstName = "Taras";
+let lastName = "Pokroveckiy";
+let group = "749";
+let birdthYear = "1984";
+
+let isMarried = true;
+
+console.log(birdthYear);
+console.log(isMarried); 
+console.log(firstName + " " + lastName + " - " + group);
+
+let nullVar = null;
+let undefinedVar;
+
+console.log(typeof nullVar); 
+console.log(typeof undefinedVar); 
+
+
+// завдання 7
+
+let name = prompt('Введіть своє імя ');
+let login = prompt('Введіть ваш логін');
+let password = prompt('Введіть ваш пароль');
+alert(`Ваше ім'я: ${name}, логін: ${login}, пароль: ${password}`);
+// завдання 8
+let secondsHour = 60*60;
+let secondsDay = secondsHour*24;
+let secondsMonth = secondsDay*30;
+
+console.log(`Секунд в годині:${secondsHour}`);
+console.log(`Секунд в добі: ${secondsDay}`);
+console.log(`секунд в місяці: ${secondsMonth}`);
